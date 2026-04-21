d5a59e2dd56197cdee215dd528dc2744e822227e
Neto <netomarim@gmail.com>
2026-04-21 12:58:27 -0300

    Alteração de detalhes pontuais

--- utils/auth_decorator.py
+++ utils/auth_decorator.py
@@ -4,8 +4,8 @@ from flask import session, redirect, url_for
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if "user_id" not in session:  # 🔥 CORRETO
-            return redirect(url_for("auth.login_page"))
+        if "user_id" not in session:
+            return redirect(url_for("views.login_page"))
         return f(*args, **kwargs)
     return decorated_function
 
@@ -13,7 +13,7 @@ def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if "user_id" not in session:
-            return redirect(url_for("auth.login_page"))
+            return redirect(url_for("views.login_page"))
 
         if session.get("tipo") != "admin":
             return "Acesso negado", 403
