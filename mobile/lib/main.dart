9d386ff786da106604c97595f7c3b3581fcd53d0
Neto <netomarim@gmail.com>
2026-08-15 14:15:36 -0300

    Adicionando câmera para teste

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const FrequenciaApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final cameras = await availableCameras();
+
+  runApp(FrequenciaApp(cameras: cameras));
 }
 
 class FrequenciaApp extends StatelessWidget {
-  const FrequenciaApp({super.key});
+  final List<CameraDescription> cameras;
+
+  const FrequenciaApp({super.key, required this.cameras});
 
   @override
   Widget build(BuildContext context) {
@@ -30,26 +37,20 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('FREQUEN.IA'), centerTitle: true),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Registro de Ponto',
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-            ),
-
-            const SizedBox(height: 40),
-
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const CameraPage()),
-                );
-              },
-              child: const Text('IR PARA CÂMERA'),
-            ),
-          ],
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CameraPage(
+                  cameras: context
+                      .findAncestorWidgetOfExactType<FrequenciaApp>()!
+                      .cameras,
+                ),
+              ),
+            );
+          },
+          child: const Text('IR PARA CÂMERA'),
         ),
       ),
     );
@@ -57,22 +58,66 @@ class HomePage extends StatelessWidget {
 }
 
 // TELA DA CÂMERA
-class CameraPage extends StatelessWidget {
-  const CameraPage({super.key});
+class CameraPage extends StatefulWidget {
+  final List<CameraDescription> cameras;
+
+  const CameraPage({super.key, required this.cameras});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  CameraController? controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initializeCamera();
+  }
+
+  Future<void> initializeCamera() async {
+    if (widget.cameras.isEmpty) {
+      return;
+    }
+
+    final camera = widget.cameras.firstWhere(
+      (camera) => camera.lensDirection == CameraLensDirection.front,
+      orElse: () => widget.cameras.first,
+    );
+
+    controller = CameraController(
+      camera,
+      ResolutionPreset.medium,
+      enableAudio: false,
+    );
+
+    await controller!.initialize();
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (controller == null || !controller!.value.isInitialized) {
+      return Scaffold(
+        appBar: AppBar(title: const Text('Câmera')),
+        body: const Center(child: CircularProgressIndicator()),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(title: const Text('Câmera'), centerTitle: true),
-
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Futuramente vamos abrir a câmera aqui.
-          },
-          child: const Text('ABRIR CÂMERA'),
-        ),
-      ),
+      body: Center(child: CameraPreview(controller!)),
     );
   }
 }
